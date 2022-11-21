5b889ea57606f81cc6035faf65af463850ca3a9b
yuumin97 <fiesportlmtc@gmail.com>
2022-11-21 09:08:53 +0700

    angular

---
+++ src/app/profile/profile/profile.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {TokenService} from '../../serivce/token.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+  name: string
+  avatar: string
+  roles: string[]
+  constructor(private router: Router,
+              private tokenService: TokenService) { }
+
+  ngOnInit(): void {
+    if (this.tokenService.getToken()){
+      this.name = this.tokenService.getName();
+      this.avatar = this.tokenService.getAvatar();
+      this.roles = this.tokenService.getRole();
+    }
+  }
+  logOut(){
+    localStorage.clear();
+    this.router.navigate(['']).then(()=>{
+      location.reload();
+    })
+  }
+}
